59d3833b3a9b56dc87861e24633ede639faa1799
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-18 13:24:24 -0500

    Update Day 0: Mean, Median, and Mode.py
    Added an optimized find_mode() function

--- 10 Days of Statistics/Day 0: Mean, Median, and Mode.py
+++ 10 Days of Statistics/Day 0: Mean, Median, and Mode.py
@@ -9,6 +9,8 @@ def find_mean(arr, N):
   return sum(num/N for num in arr)
 
 # Mode: Most commonly occuring value
+# This solution runs in O(n^2) time because of the count() function nested our num_set loop
+# This can be optimized by using a dictionary counter or hashmap (see second solution at bottom)
 def find_mode(arr):
   num_set = sorted(set(arr))
   max_count = 0
@@ -26,3 +28,11 @@ def find_median(arr, N):
     return (arr[N//2] + arr[N//2 - 1]) / 2
   else:
     return arr[N//2]
+
+# Attempting to optimize the find_mode() function
+# Using a dictionary counter reduces time complexity to O(n)
+def find_mode_optimized(arr, N):
+  frequency_dictionary = dict()
+  for num in arr:
+    frequency_dictionary[num] = frequency_dictionary.get(num, 0) + 1
+  return max(frequency_dictionary, key=frequency_dictionary.get)
